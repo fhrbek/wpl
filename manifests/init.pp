e49a3990539499e063d501965a2d86f34e9c517c
root <root@wpl.localdomain>
2013-12-29 00:03:45 +0100

    Added backup feature

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,7 +23,13 @@
 #
 # Copyright 2013 Filip Hrbek
 #
-class wpl {
+class wpl(
+  $backups              = false,
+  $sendBackups          = false,
+  $backupEmailFrom      = undef,
+  $backupEmailPassword  = undef,
+  $backupEmailRecipient = undef,
+  ) {
 
   # Install Apache Tomcat server
   include tomcat6
@@ -70,5 +76,92 @@ class wpl {
     user    => 'root',
     hour    => 4,
     minute  => 0,
+  }
+
+  if($backups) {
+    file { '/opt/wplbackup':
+      ensure => directory,
+      owner  => 'postgres',
+      mode   => '0700',
+    }
+
+    if($sendBackups) {
+      file { '/opt/gsender':
+        ensure => directory,
+        owner  => 'root',
+        mode   => '0700',
+      }
+
+      file { '/opt/gsender/gsender':
+        ensure  => present,
+        source  => 'puppet:///modules/wpl/gsender/gsender',
+        owner   => 'root',
+        mode    => '0700',
+        require => File['/opt/gsender'],
+      }
+
+      file { '/opt/gsender/lib':
+        ensure => directory,
+        owner  => 'root',
+        mode   => '0700',
+        require => File['/opt/gsender'],
+      }
+
+      file { '/opt/gsender/lib/gsender.jar':
+        ensure  => present,
+        source  => 'puppet:///modules/wpl/gsender/lib/gsender.jar',
+        owner   => 'root',
+        mode    => '0600',
+        require => File['/opt/gsender/lib'],
+      }
+
+      file { '/opt/gsender/lib/args4j-2.0.21.jar':
+        ensure  => present,
+        source  => 'puppet:///modules/wpl/gsender/lib/args4j-2.0.21.jar',
+        owner   => 'root',
+        mode    => '0600',
+        require => File['/opt/gsender/lib'],
+      }
+
+      file { '/opt/gsender/lib/mail-1.5.0-b01.jar':
+        ensure  => present,
+        source  => 'puppet:///modules/wpl/gsender/lib/mail-1.5.0-b01.jar',
+        owner   => 'root',
+        mode    => '0600',
+        require => File['/opt/gsender/lib'],
+      }
+
+      file { '/opt/wplbackup.cron':
+        ensure  => present,
+        owner   => 'root',
+        mode    => '0700', 
+        content => "TIMESTAMP=`date +%Y%m%d%H%M%S`; cd /opt/gsender; su - postgres -c \"pg_dump -f /opt/wplbackup/\$TIMESTAMP.sql.gz -Z 9 wittmannpokerleague\";./gsender -f \"${backupEmailFrom}\" -s \"WPL Backup \$TIMESTAMP\" -p \"${backupEmailPassword}\" -b \"This is a WPL database backup file.\" -a /opt/wplbackup/\$TIMESTAMP.sql.gz \"${backupEmailRecipient}\";TIMESTAMP=",
+      }
+    }
+    else {
+      file { '/opt/wplbackup.cron':
+        ensure  => present,
+        owner   => 'root',
+        mode    => '0700', 
+        content => "TIMESTAMP=`date +%Y%m%d%H%M%S`; su - postgres -c \"pg_dump -f /opt/wplbackup/\$TIMESTAMP.sql.gz -Z 9 wittmannpokerleague\";TIMESTAMP=",
+      }
+    }
+
+    cron { 'wplbackup':
+      command => '/opt/wplbackup.cron',
+      user    => 'root',
+      hour    => 5,
+      minute  => 0,
+      require => File['/opt/gsender'],
+    }
+  }
+  else {
+    file { '/opt/wplbackup.cron':
+      ensure => absent,
+    }
+
+    cron { 'wplbackup':
+      ensure => absent,
+    }
   }
 }
